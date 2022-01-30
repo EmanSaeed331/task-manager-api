76e4c5ffccc25d62787ccaf80a4ab397d79f9958
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-01-30 18:16:04 +0200

    fix

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/task-manger-api',{
 
 })
-
-
 /*     const me = new User({
         name:'Eman',
         age:25,
@@ -27,4 +25,4 @@ task.save().then((result)=>{
     console.log(result)
 }).catch((error)=>{
     console.log(error)
-}) */
+})*/
