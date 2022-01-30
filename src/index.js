6679d19d65212ae9b18ed106fdbdef364ec9d809
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-01-30 05:34:43 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
  const express = require('express')
+const User = require('./models/user')
  const app = express()
+ require('./db/mongoose')
+
  const port = process.env.PORT || 3000 
 app.use(express.json());
 app.post('/user',(req,res)=>{
-    console.log(req.body)
+    const user = new User(req.body)
+    user.save().then((result) => {
+        console.log(result);
+        res.send(user)
+        
+    }).catch((err) => {
+        console.log(err)
+    });
 
-    res.send('testing user')
 
 })
 
