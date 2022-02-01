805e0f3194d3c9feae41730bda12165c4fa8bb64
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-02-01 21:24:29 +0200

    add:update user

--- src/index.js
+++ src/index.js
@@ -85,6 +85,31 @@ app.get('/task/:id',async (req,res)=>{
         res.status(e).send(e)
     }
 })
+//Update 
+app.patch('/user/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+    
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+
+
 
  app.listen(port,()=>{
      console.log('Server is up on port ' + port)
