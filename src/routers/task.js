d90d2b3e670a25b7398597f86773ef609352c4dd
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-02-09 00:57:04 +0200

    add:sorting

--- src/routers/task.js
+++ src/routers/task.js
@@ -25,11 +25,18 @@ router.post('/tasks',auth,async (req,res)=>{
 // GET /tasks/completed=false
 // Pagination has two options : 1- limit , 2- skip .
 // GET /tasks?limit=10&skip=20
+// GET /tasks?sortBy=createdAt:desc
  router.get('/tasks', auth ,async (req,res)=>{
     const match = {}
+    const sort  = {} 
     if(req.query.completed){
         match.completed = req.query.completed ==='true'
-    } 
+    }
+     if(req.query.sortBy){
+         const parts = req.query.sortBy.split(':')
+         sort[parts[0]] = parts[1] === 'desc' ?-1 :1
+
+     }
    try{
    //const tasks = await Task.find({owner:req.user._id})
    await req.user.populate({
@@ -38,7 +45,8 @@ router.post('/tasks',auth,async (req,res)=>{
        match , 
        Options:{
            limit : parseInt(req.query.limit),
-           skip:parseInt(req.query.skip) 
+           skip:parseInt(req.query.skip) ,
+           sort
        }
 
    }).execPopulate()
