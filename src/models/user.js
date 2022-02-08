314e12030947c66129d74e16e675590882c81583
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-02-08 04:22:01 +0200

    add:delete user tasks when user is removed

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const Task = require ('../models/task')
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -92,6 +93,13 @@ userSchema.pre("save",async function(next){
     if(user.isModified('password')){
         user.password = await bcrypt.hash(user.password,8)
     }
+    next()
+})
+//Delete user tasks when user is removed . 
+userSchema.pre('remove',async function (next) {
+    const user = this
+    Task.deleteMany({owner:user._id})
+
     next()
 })
 const User = mongoose.model('User',userSchema)
