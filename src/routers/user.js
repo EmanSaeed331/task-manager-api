b46d833d24e24c1cd5ada3a62a84221680809697
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-02-09 22:12:07 +0200

    add:get avatar

--- src/routers/user.js
+++ src/routers/user.js
@@ -157,7 +157,18 @@ router.delete('/users/me/avatar',auth,async (req,res)=>{
     await req.user.save()
     res.send()
 })
-
+//Get avatar 
+router.get('/users/:id/avatar',async(req,res)=>{
+    try{
+        const user = await User.findById(req.params.id)
+        if(!user || !user.avatar){
+            throw new Error()
+        }
+        res.set('Content-Type','image/jpg')
+        res.send(user.avatar)
+    }
+    catch(e){}
+})
 
 
 module.exports =  router
