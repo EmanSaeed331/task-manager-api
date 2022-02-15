c42c6a534fab2c573d134536160853f115f337dd
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-02-15 04:31:18 +0200

    test:http response body

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,31 +1,60 @@
 const request = require('supertest')
 const app = require('../src/app')
 const User = require('../src/models/user')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 
+const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
+    _id:userOneId,
     name:'Emma',
     email:'emansaeed133@gmail.com',
-    password:'1100Emmma1100'
+    password:'1100Emmma1100',
+    tokens:[
+        {
+            token:jwt.sign({_id:userOneId},process.env.JWT_SECRET)
+        }
+    ]
 }
 beforeEach(async ()=>{
      await User.deleteMany()
      await new User(userOne).save() 
 })
+/*
+ ########### HTTP Response status code test ###########
+*/
+/*
+ ########### HTTP Response body  test ###########
+*/
 //testing create user 
 test('Sign Up a new User',async ()=>{
-    await request(app).post('/users').send({
+    const response = await request(app).post('/users').send({
         name:'Eman',
         email:'EmanSaeed5330@gmail.com',
         password:'Eman@0101010!!',
 
     }).expect(201)
+    // assert that the database was changed correctly //
+    const user = await  User.findById(response.body.user._id)
+    expect(user).not.toBeNull()
+    // Assertions about the response 
+    expect(response.body).toMatchObject({
+        user:{
+            name:'Eman',
+            email:'EmanSaeed5330@gmail.com',
+        },
+        token:user.tokens[0].token
+    })
+    expect(user.password).not.toBe('Eman@0101010!!')
 })
 //testing login 
 test('Should login existing user', async()=>{
-    await request(app).post('/users/login').send({
+    const response = await request(app).post('/users/login').send({
         email:userOne.email,
         password:userOne.password,
     }).expect(200)
+    const user = await User.findById(userOneId)
+    expect(response.body.token).toBe(user.tokens[1].token)
 })
 
 // test login failure 
@@ -35,3 +64,39 @@ test('Should not login not existing user', async()=>{
         password:'testPass010101010',
     }).expect(400)
 })
+// test user fetching profile 
+// confirm if the server confirm that token --> it is not expired  
+test('Should get profile for user ', async() =>{
+    await request(app)
+    .get('/users/me')
+    .set('Authorization',`Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200)
+})
+test('Should not get profile for unauthenticated user', async()=>
+{
+    await request(app)
+    .get('/users/me')
+    .send()
+    .expect(401)
+})
+
+//test delete account 
+test('Should delete account for user', async() =>{
+    await request(app)
+    .delete('/user/me')
+    .set('Authorization',`Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200) 
+    const user = await User.findById(userOneId)
+    expect(user).toBeNull()
+
+})
+test('Should not delete account unauthenticated user', async() =>{
+    await request(app)
+    .delete('/user/me')
+    .send()
+    .expect(401)
+
+})
+
